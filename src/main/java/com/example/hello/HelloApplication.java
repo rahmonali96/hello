5e82a2006720d4f4287a5eac79407e7b5d7482b8
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-08-04 13:59:36 +0500

    2022-08-04-7

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class HelloApplication {
-    @GetMapping("/test")
+    @GetMapping("/")
     public String test() {
         return "Jenkins is working!!!";
     }
