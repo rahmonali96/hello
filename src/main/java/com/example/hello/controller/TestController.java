0a59c03bfc7da9157d8d5069529cb2276e73c9ab
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-08-18 14:41:11 +0500

    2022-08-18-19

---
+++ src/main/java/com/example/hello/controller/TestController.java
@@ -0,0 +1,39 @@
+package com.example.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.TimeZone;
+
+/**
+ * Developed by Rahmonali Yoqubov
+ * Email: rahmonaliyoqubov@gmail.com
+ * Date: 04.08.2022
+ * Project: hello
+ */
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public Message test(HttpServletRequest request) {
+        System.out.println("hi");
+        return new Message("Your ip: " + request.getRemoteHost()+"   User-Agent: "+request.getHeader("User-Agent"));
+    }
+
+    private static class Message{
+        private String message;
+
+        public Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
