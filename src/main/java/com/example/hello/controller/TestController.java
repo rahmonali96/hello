1411301c6a6e98fd6b01aff923faed60c8fcd23b
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-08-18 10:21:41 +0500

    2022-08-18-2

--- src/main/java/com/example/hello/controller/TestController.java
+++ src/main/java/com/example/hello/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.hello.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,10 @@ import java.util.TimeZone;
  */
 @RestController
 public class TestController {
+    private static final Logger logger = LogManager.getLogger(TestController.class);
     @GetMapping("/test")
     public Message test(HttpServletRequest request) {
+        logger.info("Ip: " + request.getRemoteHost());
         return new Message("Your ip: " + request.getRemoteHost()+"   User-Agent: "+request.getHeader("User-Agent"));
     }
 
